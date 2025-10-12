b440f9961dcedccd70842c10fe1fd52329d73661
yashrahmed <yashrahmed@gmail.com>
2025-10-12 09:47:38 -0500

    (kind of) understanding dspy predict module

--- chai_gpt_v7/dspy_test.py
+++ chai_gpt_v7/dspy_test.py
@@ -22,11 +22,24 @@ def llm_cot():
     response = math_bot(question="Two dice are tossed. What is the probability that the sum equals two?")
     print(response)
 
+def llm_predict_test():
+    llm = dspy.LM("gpt-5-nano", temperature=1.0, max_tokens=16000)
+    dspy.configure(lm=llm)
+    # Prompt generated within the chat adapter.
+    # There is no restriction on the values used in the text signature as long as the LLM can make sense of it after the chat message is rendered.
+    # Inputs are to be supplied with the same kwarg in the callable see 'car' below.
+    # Outputs can be anything as long as the LLM can make sense of it i.e using car -> xyz won't work since in the actual chat message, the llm cannot tell what xyz is.
+    predict = dspy.Predict("car -> length_in_meters:float")
+    response = predict(car="Lamborghini Murciélago")
+    print('___________________')
+    print(response)
+
 
 def main():
     set_open_api_key()
     # call_an_llm()
-    llm_cot()
+    # llm_cot()
+    llm_predict_test()
 
 if __name__ == '__main__':
     main()
